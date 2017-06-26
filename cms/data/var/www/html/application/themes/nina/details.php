1033de572ddbe088253d054f7127cd07f85a06c6
onishi-nina <y.onishi.nina@gmail.com>
2017-06-27 08:48:36 +0900

    デフォルトのテンプレートを追加

--- cms/data/var/www/html/application/themes/nina/details.php
+++ cms/data/var/www/html/application/themes/nina/details.php
@@ -2,40 +2,41 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $this->inc('elements/header.php'); 
 ?>
+
 <div id="outercontainer1">
 
-	<div id="outercontainer2">
+<div id="outercontainer2">
 
 
 
-		<div id="container">
+<div id="container">
 
-			<div id="contents">
+ <div id="contents">
 
-				<div id="furisode-detail">
+      <div id="furisode-detail">
 
-					<?php
+         <?php
 
-					$a = new Area('Main');
+         $a = new Area('Main');
 
-					$a->display($c);
-					
-					?>
-					
-				</div>
-
-
-				<div style="clear:both">
+         $a->display($c);
+		
+         ?>
+     
+  </div>
 
 
-				</div>
-			</div>
-
-		</div>
-
-	</div>
+<div style="clear:both">
 
 
-	<?php
-	$this->inc('elements/footer.php'); 
-	?>
+ </div>
+</div>
+
+</div>
+
+</div>
+
+
+<?php
+$this->inc('elements/footer.php'); 
+?>
