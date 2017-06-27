542cb917c6e469252943fb2acf828443423ae57e
onishi-nina <y.onishi.nina@gmail.com>
2017-06-28 00:48:00 +0900

    料金ページのテンプレートのパスを修正

--- cms/data/var/www/html/application/themes/nina/flow_entry.php
+++ cms/data/var/www/html/application/themes/nina/flow_entry.php
@@ -66,15 +66,15 @@ $this->inc('elements/header.php');
     <ul class="p-related__item">
 
       <li class="p-related__item__box c-thumbnail--large">
-        <p class="c-thumbnail--large__item--img"><img src="/img/article/01.jpg"></p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/01.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">制作依頼で失敗しない５つのポイントをまとめてみました。</p>
       </li>
       <li class="p-related__item__box c-thumbnail--large">
-        <p class="c-thumbnail--large__item--img"><img src="/img/article/02.jpg"></p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/02.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">最近のお仕事でやっていることを公開します。</p>
       </li>
       <li class="p-related__item__box c-thumbnail--large">
-        <p class="c-thumbnail--large__item--img"><img src="/img/article/03.jpg"></p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/03.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
       </li>
 
