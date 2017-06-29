cb12a8ef8cc3e0a4e731f6e40a48001127ca5f39
onishi-nina <y.onishi.nina@gmail.com>
2017-06-30 00:40:01 +0900

    料金ページと仕事の流れのテンプレートファイルにブロックタグを埋め込み

--- cms/data/var/www/html/application/themes/nina/price_entry.php
+++ cms/data/var/www/html/application/themes/nina/price_entry.php
@@ -7,142 +7,23 @@ $this->inc('elements/header.php');
 
   <span class="p-bg--item--primary--under c-square--primary"></span> <span class="p-bg--item--secondary--under c-square--tertiary"></span>
 
-
   <div class="l-price">
 
-    <p class="l-price__ttl c-contentTtl--secondary">料金について</p>
-
-    <h2 class="l-price__cache">プロジェクトの形態や規模・期間に応じて柔軟にご提案します</h2>
-    <p class="l-price__text--primary">プロジェクトの形態や規模、期間に応じて柔軟にご提案させていただきます。</p>
-
-    <ul class="l-price__nav">
-      <li><a href="#ui">UIデザイン</a></li>
-      <li><a href="#respon">レスポンシブデザイン</a></li>
-      <li><a href="#web">ウェブコンサルティング</a></li>
-    </ul>
-    <p class="l-price__text--secondary">お見積やご相談は無料です。お気軽にどうぞ。 </p>
-
-
-
-    <div class="l-price__inner">
-
-      <h3 id="ui" class="l-price__inner__ttl">UIデザイン</h3>
-      <p class="l-price__inner__text">サービスへの理解を深め、UX設計や事業戦略から運用まで一緒に考えていきます。クライアントがコンテンツを通して実現したいこと・目的に対してユーザーを誘導する手段としてのデザインを提案します。</p>
-      <p class="l-price__inner__text">デザインツールはPHOTOSHOPの他、SKETCHアプリの対応も可能です。</p>
-
-      <div class="l-price__inner__table">
-        <dl class="l-price__inner__table__col">
-          <dt>期間</dt>
-          <dd>要相談</dd>
-        </dl>
-        <dl class="l-price__inner__table__col">
-          <dt>費用</dt>
-          <dd>1プロジェクト&nbsp;:&nbsp;60万円〜</dd>
-        </dl>    
-      </div>
-      <p class="l-price__inner__text--secondary">東京23区近郊の常駐もお受けいたします。</p>
-      <div class="l-price__inner__btn">
-        <p class="c-btn--secondary">
-          <a href="/service/design" title="">UIデザインの詳細はこちら</a>
-        </p>
-        
-      </div>
-
-      
-
-
-    </div>
-
-    <div class="l-price__inner">
-
-      <h3 id="respon" class="l-price__inner__ttl">レスポンシブWebデザインサイト</h3>
-      <p class="l-price__inner__text">ご依頼をいただき次第、実現したい目的やご要望のヒアリングを基にデザインとコーディングファイルの納品まで対応します。</p>
-      <p class="l-price__inner__text">CSSアニメーションを利用し、パフォーマンスと見栄えに優れた、モバイル環境でも読み込み速度が速いWebサイトを納品。</p>
-      <div class="l-price__inner__table">
-        <dl class="l-price__inner__table__col">
-          <dt>期間</dt>
-          <dd>10営業日〜</dd>
-        </dl>
-        <dl class="l-price__inner__table__col">
-          <dt>費用</dt>
-          <dd>10ページ&nbsp;:&nbsp;50万円〜</dd>
-        </dl>    
-      </div>
-      <p class="l-price__inner__text--secondary">ご依頼後、サイト診断・コンセプトに沿いサイト公開時に必要なサポートをお任せください。</p>
-      <div class="l-price__inner__btn">
-        <p class="c-btn--secondary">
-          <a href="/service/responsive" title="">レスポンシブWebデザインの詳細はこちら</a>
-        </p>
-        
-      </div>
-
-      
-
-
-    </div>
-
-    <div class="l-price__inner">
-
-      <h3 id="web" class="l-price__inner__ttl">ウェブコンサルティング</h3>
-      <p class="l-price__inner__text">アクセス解析や競合分析による自社・他社を含むサイト内外の「現状分析」を診断します。</p>
-
-      <p class="l-price__inner__text">そこから問題点を洗い出し、プロトタイプやカスタマージャーニーマップといったデザイン的な戦略手法を使いマーケティング活動をサポートします。</p>
-      <div class="l-price__inner__table">
-        <dl class="l-price__inner__table__col">
-          <dt>期間</dt>
-          <dd>3ヶ月以上〜</dd>
-        </dl>
-        <dl class="l-price__inner__table__col">
-          <dt>費用</dt>
-          <dd>1ヶ月&nbsp;:&nbsp;10万円から〜</dd>
-        </dl>    
-      </div>
-      <p class="l-price__inner__text--secondary">ご依頼後、サイト診断・コンセプトに沿いサイト公開時に必要なサポートをお任せください。</p>
-      <div class="l-price__inner__btn">
-        <p class="c-btn--secondary">
-          <a href="/service/support" title="">ウェブコンサルティングの詳細はこちら</a>
-        </p>
-        
-      </div>
-
-      
-
-
-    </div>
-
-    <div class="l-price__item--cta c-cta">
-
-      <p class="c-cta__ttl">お仕事のご依頼はこちらでお受けします</p>
-
-      <div class="c-cta__inner">
-        <p class="c-cta__inner__col">レスポンシブWebデザインサイト構築・コーポレートサイトやサービス画面のUIデザインなど、お気軽にお問い合わせください。</p>
-        <p class="c-cta__inner__col">2営業日以内にご連絡いたします。</p>
-        <p class="c-cta__inner__btn c-btn--secondary--orange"><a href="#">現在コンタクトフォームは停止しています</a></p>
-      </div>
-
-    </div>
-
-
-
+    <?php
+    $a = new Area('Main');
+    $a->enableGridContainer();
+    $a->display($c);
+    ?>
 
   </div>
-
-
-  
-  
 </div>
 </div>
 
 <div class="l-content--secondary">
-
   <div class="p-related">
-
     <p class="p-related__cache">あなたにオススメの記事</p>
     <p class="p-related__text--attention">当サイトは発信型メディアとして、近日リニューアルします。</p>
-
-
     <ul class="p-related__item">
-
       <li class="p-related__item__box c-thumbnail--large">
         <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/01.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">制作依頼で失敗しない５つのポイントをまとめてみました。</p>
@@ -155,12 +36,8 @@ $this->inc('elements/header.php');
         <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/03.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
       </li>
-
-
     </ul>
   </div>
-
-
 </div>
 
 <?php
