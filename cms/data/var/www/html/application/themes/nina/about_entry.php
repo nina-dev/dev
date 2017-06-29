49f98cb4065a3618cdf447f127a5a03b8082b99f
onishi-nina <y.onishi.nina@gmail.com>
2017-06-29 23:29:35 +0900

    aboutページのテンプレートにブロックタグを埋め込み

--- cms/data/var/www/html/application/themes/nina/about_entry.php
+++ cms/data/var/www/html/application/themes/nina/about_entry.php
@@ -7,105 +7,13 @@ $this->inc('elements/header.php');
 
  <span class="p-bg--item--primary--under c-square--primary"></span> <span class="p-bg--item--secondary--under c-square--tertiary"></span>
 
-
  <div class="l-about">
 
-   <p class="l-about__ttl c-contentTtl--secondary">ニナ・アーキタイプについて</p>
-
-   <h2 class="l-about__cache">制作プロダクションを経てフリーランスとして独立しました</h2>
-
-   <p class="l-about__img"><img src="<?php echo $this->getThemePath(); ?>/img/about/01.png" alt=""></p>
-
-
-   <div class="l-about__inner">
-
-     <p>ニナ・アーキタイプは都内の制作プロダクション数社を経て、2016年にフリーランスとして独立したデザインマネジメントを得意とするUIデザイナーです。</p>
-     <h3>目的や課題解決のための手法がデザインです</h3>
-
-     <p>UIデザイナー。従来のWebデザイナー・ディレクターとの違いって？と思う人もいると思います。僕が考えるUIデザイナーの定義は、従来のデザイナー・ディレクター以上に、使い易さや操作感の設計に力をいれ、<b>クライアントがコンテンツを通して実現したいこと・目的に対してユーザーを誘導することに特化させたデザイナー</b>という認識を持っています。</p>
-     <h3>サービスやコンテンツの目的を理解することに注力します</h3>
-     <p><b>デザインはつくることが目的ではなく、あくまで目的解決の為の手段</b>です。ひらめきで素晴らしいセンスを発揮する見栄えの良い"だけ"のデザインでは、意味も価値もありません。</p>
-     <p>クライアントのコンテンツへの理解を深めユーザーにとってわかりやすく、使いやすくすること。ユーザーにポジティブな印象を与えること。コンテンツがユーザーに伝わり、結果として繋がりつくる手段としてのデザインをクライアントと一緒に考え、提供します。</p><p>クライアント自身のビジネスの実情を踏まえた上で的確な表現やご提案をいたしますので、ぜひお問い合わせください。</p>
-   </div>
-
-   <div class="l-about__item">
-     <h4 class="l-about__item__ttl">ニナ・アーキタイプ</h4>
-
-     <ul class="l-about__item__detail">
-       <li>
-         <dl class="c-table">
-           <dt>名称</dt>
-           <dd>nina ARCHETYPE (ニナ・アーキタイプ)</dd>
-         </dl>
-       </li>
-       <li>
-        <dl class="c-table">
-          <dt>代表者名</dt>
-          <dd>藤岡&nbsp;浩史</dd>
-        </dl>
-      </li>
-
-      <li>
-       <dl class="c-table">
-         <dt>所在地</dt>
-         <dd>東京都世田谷区下馬(※自宅兼用のため請求書等に記載）</dd>
-       </dl>
-     </li>
-     <li>
-       <dl class="c-table">
-         <dt>連絡先</dt>
-         <dd>info@nina-archetype.com</dd>
-       </dl>
-     </li>
-     <li>
-       <dl class="c-table">
-         <dt>業務内容</dt>
-         <dd>
-           ブランディング・コミュニケーションデザイン／総合的なコミュニケーションデザインの企画・デザイン／C.I. / V.I. / ブランディングの企画・デザイン／販促プロモーションの企画・デザイン／Webサイトの企画・デザインおよび制作／アプリケーション・プロモーションツールのデザインおよび制作／ポスター・パンフレット・カタログ・パッケージ／ディスプレイ・媒体広告などのデザインおよび制作</dd>
-         </dl>
-       </li>
-
-     </ul>
-
-   </div>
-
-
-   <div class="l-about__item c-profile">
-     <h4 class="l-about__item__ttl">プロフィール</h4>
-     <div class="l-about__item__detail c-profile__inner">
-       <div class="c-profile__inner__item--img">
-         <img src="<?php echo $this->getThemePath(); ?>/img/about/img01.png" alt="">
-       </div>
-       <div class="c-profile__inner__item--detail">
-         <p class="c-profile__inner__item__ttl">藤岡&nbsp;浩史</p>
-         <p class="c-profile__inner__item__subtext">UIデザイナー・Webデザイナー・Webコンサルタント・UXリサーチャー</p>
-         <p class="c-profile__inner__item__text">
-           東京都世田谷区在住。横浜市出身。
-         </p>
-         <p class="c-profile__inner__item__text">都内数社の制作プロダクションを経て、2016年8月に独立しました。</p>
-         <p class="c-profile__inner__item__text">これまでWebサイトやアプリ上におけるフロントエンド・デザインを現場レベルで数多く経験しています。</p>
-
-         <p class="c-profile__inner__item__text">単純な「つくる」だけでなくユーザー目線から逆算し、クライアントはもちろんコンテンツやブランドに付加価値を与える事ができる仕事を目指しています。</p>
-       </div>
-
-       
-     </div>
-
-   </div>
-
-   <div class="l-about__item--cta c-cta">
-
-     <p class="c-cta__ttl">お仕事のご依頼はこちらでお受けします</p>
-
-     <div class="c-cta__inner">
-       <p class="c-cta__inner__col">レスポンシブWebデザインサイト構築・コーポレートサイトやサービス画面のUIデザインなど、お気軽にお問い合わせください。</p>
-       <p class="c-cta__inner__col">2営業日以内にご連絡いたします。</p>
-       <p class="c-cta__inner__btn c-btn--secondary--orange"><a href="#">現在コンタクトフォームは停止しています</a></p>
-     </div>
-
-   </div>
-
-
+<?php
+$a = new Area('Main');
+$a->enableGridContainer();
+$a->display($c);
+?>
 
  </div>
 </div>
@@ -118,7 +26,6 @@ $this->inc('elements/header.php');
     <p class="p-related__text--attention">当サイトは発信型メディアとして、近日リニューアルします。</p>
 
     <ul class="p-related__item">
-
       <li class="p-related__item__box c-thumbnail--large">
         <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/01.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">制作依頼で失敗しない５つのポイントをまとめてみました。</p>
@@ -131,12 +38,8 @@ $this->inc('elements/header.php');
         <p class="c-thumbnail--large__item--img"><img src="<?php echo $this->getThemePath(); ?>/img/article/03.jpg"></p>
         <p class="c-thumbnail--large__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
       </li>
-
-
     </ul>
   </div>
-
-
 </div>
 
 <?php
