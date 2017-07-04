a3c8f70af0c7d9d8997005cba6e051905c2939ff
onishi-nina <y.onishi.nina@gmail.com>
2017-07-04 21:59:00 +0900

    footerの記述を修正

--- cms/data/var/www/html/application/themes/nina/elements/footer.php
+++ cms/data/var/www/html/application/themes/nina/elements/footer.php
@@ -1,17 +1,18 @@
 <?php
 defined('C5_EXECUTE') or die(_("Access Denied."));?>
 
-<footer class="l-footer">
-  <p class="l-footer__item--left">COPYRIGHT nina&nbsp;archetype.com</p>
+<footer class="l-footer--lower">
+	<p class="l-footer__item--left">COPYRIGHT nina&nbsp;archetype.com</p>
 
-  <div class="l-footer__item--right c-pagetop">
-    <span class="c-pagetop__item"></span>
-  </div>
+	<div class="l-footer__item--right c-pagetop">
+		<span class="c-pagetop__item"></span>
+	</div>
 
 </footer>
 </div>
 
-  <script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/script.js"></script>
+
+<script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/script.js"></script>
 
 <?php  Loader::element('footer_required'); ?>
 
--- cms/data/var/www/html/application/themes/nina/elements/footer_top.php
+++ cms/data/var/www/html/application/themes/nina/elements/footer_top.php
@@ -7,27 +7,27 @@ defined('C5_EXECUTE') or die(_("Access Denied."));?>
   <div class="l-footer__item--right c-pagetop">
     <span class="c-pagetop__item"></span>
   </div>
-
 </footer>
 </div>
 
-  <script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/script.js"></script>
-    <script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/particles.min.js"></script>
-    <script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/app.js"></script>
 
-  <script type="text/javascript">
+<script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/script.js"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/particles.min.js"></script>
+<script type="text/javascript" src="<?php echo $this->getThemePath()?>/js/app.js"></script>
 
-    jQuery(window).load(function() {
-     $(".l-loading").addClass('is-anime');
-     $(".l-container").hide();
+<script type="text/javascript">
 
-     setTimeout(function(){
-       $(".l-container").addClass('is-open');
-       $(".l-loading").hide();
+  jQuery(window).load(function() {
+   $(".l-loading").addClass('is-anime');
+   $(".l-container").hide();
 
-     },2400);
-   });
- </script>
+   setTimeout(function(){
+     $(".l-container").addClass('is-open');
+     $(".l-loading").hide();
+
+   },2400);
+ });
+</script>
 
 
 <?php  Loader::element('footer_required'); ?>
