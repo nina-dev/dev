49a66880bd037c59a674f4e4cc0c1833f8471f40
nina-fujioka <h.fujioka.nina@gmail.com>
2017-07-21 02:38:25 +0900

    #72 sidebarのスタイル追加

--- wp/wordpress/wp-content/themes/nina/sidebar.php
+++ wp/wordpress/wp-content/themes/nina/sidebar.php
@@ -1,3 +1,248 @@
-<div class="l-content--lower__item--right l-side">
-テキストテキストテキストテキストテキストテキスト
+<div class="l-content--lower__item--right l-sidebar">
+    <div class="l-sidebar__col">
+        <div class="c-introduce">
+            <p class="c-introduce__text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </div>
+    </div>
+    <div class="l-sidebar__col">
+        <p class="l-sidebar__col__ttl">カテゴリー一覧</p>
+        <ul class="l-sidebar__col__item c-list--category">
+            <li>
+                <a href="#">カテゴリーネーム</a>
+            </li>
+            <li>
+                <a href="#">カテゴリーネーム</a>
+            </li>
+            <li>
+                <a href="#">カテゴリーネーム</a>
+            </li>
+        </ul>
+    </div>
+    <div class="l-sidebar__col">
+        <p class="l-sidebar__col__ttl">タグ一覧</p>
+        <ul class="l-sidebar__col__item c-list--tag">
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+            <li>
+                <a href="#">タグネーム</a>
+            </li>
+        </ul>
+    </div>
+    <div class="l-sidebar__col">
+        <p class="l-sidebar__col__ttl">関連記事</p>
+        <ul class="l-sidebar__col__item">
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <ul class="c-thumbnail--small__item__list c-list--tag">
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <ul class="c-thumbnail--small__item__list c-list--tag">
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <ul class="c-thumbnail--small__item__list c-list--tag">
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <ul class="c-thumbnail--small__item__list c-list--tag">
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <ul class="c-thumbnail--small__item__list c-list--tag">
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                        <li>
+                            <a href="#">タグネーム</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="l-sidebar__col">
+        <p class="l-sidebar__col__ttl">最新記事</p>
+        <ul class="l-sidebar__col__item">
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <div class="c-thumbnail--small__item__list">
+                    <p class="c-thumbnail--small__item__list__text">yyyy/mm/dd</p>
+                    </div>
+                </div>
+            </li>
+
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <div class="c-thumbnail--small__item__list">
+                    <p class="c-thumbnail--small__item__list__text">yyyy/mm/dd</p>
+                    </div>
+                </div>
+            </li>
+
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <div class="c-thumbnail--small__item__list">
+                    <p class="c-thumbnail--small__item__list__text">yyyy/mm/dd</p>
+                    </div>
+                </div>
+            </li>
+
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <div class="c-thumbnail--small__item__list">
+                    <p class="c-thumbnail--small__item__list__text">yyyy/mm/dd</p>
+                    </div>
+                </div>
+            </li>
+
+            <li class="l-sidebar__col__item__list c-thumbnail--small">
+                <div class="c-thumbnail--small__item--img">
+                    <a href="#">
+                        <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                    </a>
+                </div>
+                <div class="c-thumbnail--small__item--detail">
+                    <p class="c-thumbnail--small__item__ttl">
+                        <a href="#">タイトルタイトルタイトルタイトルタイトルタイトルタイトル</a>
+                    </p>
+                    <div class="c-thumbnail--small__item__list">
+                    <p class="c-thumbnail--small__item__list__text">yyyy/mm/dd</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
