30cf27a3c92cf6ec295923414c99ef01270bd2bf
nina-fujioka <h.fujioka.nina@gmail.com>
2017-07-23 00:04:25 +0900

    related箇所のレイアウト調整

--- wp/wordpress/wp-content/themes/nina/template_parts/p-related.php
+++ wp/wordpress/wp-content/themes/nina/template_parts/p-related.php
@@ -1,22 +1,28 @@
 <div class="p-related">
   <p class="p-related__cache">あなたにオススメの記事</p>
   <ul class="p-related__item">
-    <li class="p-related__item__box c-thumbnail--largeborder">
+    <li class="p-related__item__box c-thumbnail--large">
       <a href="#">
-        <p class="c-thumbnail--largeborder__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/01.jpg"></p>
-        <p class="c-thumbnail--largeborder__item--ttl--related">制作依頼で失敗しない５つのポイントをまとめてみました。</p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/01.jpg"></p>
+        <p class="c-thumbnail--large__item--ttl--related">制作依頼で失敗しない５つのポイントをまとめてみました。</p>
       </a>
     </li>
-    <li class="p-related__item__box c-thumbnail--largeborder">
+    <li class="p-related__item__box c-thumbnail--large">
       <a href="#">
-        <p class="c-thumbnail--largeborder__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/02.jpg"></p>
-        <p class="c-thumbnail--largeborder__item--ttl--related">最近のお仕事でやっていることを公開します。</p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/02.jpg"></p>
+        <p class="c-thumbnail--large__item--ttl--related">最近のお仕事でやっていることを公開します。</p>
       </a>
     </li>
-    <li class="p-related__item__box c-thumbnail--largeborder">
+    <li class="p-related__item__box c-thumbnail--large">
       <a href="#">
-        <p class="c-thumbnail--largeborder__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/03.jpg"></p>
-        <p class="c-thumbnail--largeborder__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/03.jpg"></p>
+        <p class="c-thumbnail--large__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
+      </a>
+    </li>
+    <li class="p-related__item__box c-thumbnail--large">
+      <a href="#">
+        <p class="c-thumbnail--large__item--img"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/article/03.jpg"></p>
+        <p class="c-thumbnail--large__item--ttl--related">いまWebディレクターに求められる「解析提案力」</p>
       </a>
     </li>
   </ul>
