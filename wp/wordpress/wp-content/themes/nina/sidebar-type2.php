4586b241c0539b0bd593d5b78a8a76212fd9a971
onishi-nina <y.onishi.nina@gmail.com>
2017-07-26 22:43:18 +0900

    関連記事を最新記事の表示に変更

--- wp/wordpress/wp-content/themes/nina/sidebar-type2.php
+++ wp/wordpress/wp-content/themes/nina/sidebar-type2.php
@@ -67,6 +67,48 @@
     </div>
 
     <div class="l-sidebar__col">
+        <p class="l-sidebar__col__ttl">最新記事</p>
+        <ul class="l-sidebar__col__item">
+            <?php query_posts('posts_per_page=4'); ?>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <li class="l-sidebar__col__item__list c-thumbnail--small">
+                    <div class="c-thumbnail--small__item--img">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="photo">
+                                <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき ?>
+                                    <?php the_post_thumbnail(); ?>
+                                <?php else: // サムネイルを持っていないとき ?>
+                                    <img src="http://nina.dev/wp-content/themes/nina/img/article/01.jpg">
+                                <?php endif; ?>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="c-thumbnail--small__item--detail">
+                        <p class="c-thumbnail--small__item__ttl">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php
+                                        // 文字数制限
+                                if(mb_strlen($post->post_title, 'UTF-8')>28){
+                                    $title= mb_substr($post->post_title, 0, 28, 'UTF-8');
+                                    echo $title.'・・・';
+                                }else{
+                                    echo $post->post_title;
+                                }?>
+
+                            </a>
+                        </p>
+                        <div class="c-thumbnail--small__item__list">
+                            <p class="c-thumbnail--small__item__list__text"><?php the_time('Y/m/d'); ?></p>
+                        </div>
+                    </div>
+                </li>
+            <?php endwhile; endif; ?>
+            <?php wp_reset_query(); ?>
+        </ul>
+    </div>
+
+    <!-- 記事が充実してから関連記事を表示
+    <div class="l-sidebar__col">
         <p class="l-sidebar__col__ttl">関連記事</p>
         <ul class="l-sidebar__col__item">
             <?php
@@ -125,5 +167,5 @@
                     wp_reset_postdata();
                     ?>
                 </ul>
-            </div>
+            </div> -->
         </div>
