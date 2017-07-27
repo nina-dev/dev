e87fe16b719bfdaab958ed39f974bd589aa55e49
onishi-nina <y.onishi.nina@gmail.com>
2017-07-28 00:59:57 +0900

    動的ページ表示に伴う修正

--- wp/wordpress/wp-content/themes/nina/404.php
+++ wp/wordpress/wp-content/themes/nina/404.php
@@ -1,7 +1,23 @@
 <?php get_header(); ?>
 
 <div class="l-content--lower">
-404ページ
+
+<div class="l-content--lower__inner">
+
+<div class="l-content--lower__item--left">
+
+<?php get_template_part('/template_parts/p-breadcrumb' ); ?>
+
+<?php get_template_part('/template_parts/p-article' ); ?>
+
+</div>
+
+<div class="l-content--lower__item--right">
+<?php get_sidebar('type1'); ?>
+</div>
+
+</div>
 </div>
 
 <?php get_footer(); ?>
+
