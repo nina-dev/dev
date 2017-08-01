60214a33010d52201a9857cb9fc85158b56502b7
onishi-nina <y.onishi.nina@gmail.com>
2017-08-02 03:57:32 +0900

    フォーム用ベーステンプレートを作成

---
+++ wp/wordpress/wp-content/themes/nina/page-contact.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: お問い合わせテンプレート
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="l-content--lower">
+
+	<div class="l-content--lower__inner">
+
+		<div class="l-content--lower__item--left">
+
+			<?php get_template_part('/template_parts/p-breadcrumb' ); ?>
+
+			<?php get_template_part('/template_parts/p-contact' ); ?>
+
+		</div>
+
+		<div class="l-content--lower__item--right">
+			<?php get_sidebar('type1'); ?>
+		</div>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
