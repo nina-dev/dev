db9290d1d1d5cba1fb135f4c83cc0dcdb7a3683f
onishi-nina <y.onishi.nina@gmail.com>
2017-08-03 01:02:35 +0900

    #141 コンタクトページの各ページを作成

--- wp/wordpress/wp-content/themes/nina/template_parts/p-form.php
+++ wp/wordpress/wp-content/themes/nina/template_parts/p-form.php
@@ -4,6 +4,30 @@
 				while ( have_posts() ) : the_post();
 			?>
 
+			<style>
+				.c-status {
+					width:100%;
+					background:#F2F2F2;
+					display: table;
+					text-align: center;
+				}
+				.c-status__col {
+					width:33.3333%;
+					display: table-cell;
+					padding: 16px 0;
+				}
+
+				.c-status__col.is_active {
+					background: #666;
+				}
+			</style>
+
+			<ul class="c-status">
+				<li class="c-status__col">入力画面</li>
+				<li class="c-status__col is_active">確認画面</li>
+				<li class="c-status__col">完了画面</li>
+			</ul>
+
 			<?php the_content(); ?>
 
 			<?php
