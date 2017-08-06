05f1f4171c5e2644fedb1f6308d593d68a4b025b
onishi-nina <y.onishi.nina@gmail.com>
2017-08-06 16:50:15 +0900

    formのボタンのcssを調整

--- resource/assets/js/c-form.js
+++ resource/assets/js/c-form.js
@@ -1,20 +1,24 @@
 $(function(){
-  $('.c-form').each(function() {
-      //focusしたらプレースフォルダー文言を表示・非表示
-      $(this).find('.c-form--text').focus(function(){
-          $(this).parent().parent().find('label').find('.c-form--sampletext').addClass('c-form--sampletext--show');
-      })
-      .blur(function(){
-          $(this).parent().parent().find('label').find('.c-form--sampletext').removeClass('c-form--sampletext--show');
-      });
 
-      //textボックスの値が空の場合、エラー文言を表示・非表示
-      $(this).find('.c-form--text').blur(function(){
-          if($(this).val() == ''){
-              $(this).next('.c-form--attention').addClass('c-form--attention--show');
-          }else{
-              $(this).next('.c-form--attention').removeClass('c-form--attention--show');
-          }
-      })
-  });
+    //buttonタグごとにHTMLタグを追加
+    $('button').wrap('<li class="p-form__btn__item c-cta__inner__btn c-btn--secondary--orange"></div>');
+
+    //focusしたらプレースフォルダー文言を表示・非表示
+    $('.c-form').each(function() {
+        $(this).find('.c-form--text').focus(function(){
+            $(this).parent().parent().find('label').find('.c-form--sampletext').addClass('c-form--sampletext--show');
+        })
+        .blur(function(){
+            $(this).parent().parent().find('label').find('.c-form--sampletext').removeClass('c-form--sampletext--show');
+        });
+
+        //textボックスの値が空の場合、エラー文言を表示・非表示
+        $(this).find('.c-form--text').blur(function(){
+            if($(this).val() == ''){
+                $(this).next('.c-form--attention').addClass('c-form--attention--show');
+            }else{
+                $(this).next('.c-form--attention').removeClass('c-form--attention--show');
+            }
+        })
+    });
 })
