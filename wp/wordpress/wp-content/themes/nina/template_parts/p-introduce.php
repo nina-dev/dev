405a23fae725851776363423a22217c40ad916bc
nina-fujioka <h.fujioka.nina@gmail.com>
2017-08-07 02:13:01 +0900

    #140　サイドctaエリアの修正

--- wp/wordpress/wp-content/themes/nina/template_parts/p-introduce.php
+++ wp/wordpress/wp-content/themes/nina/template_parts/p-introduce.php
@@ -1,8 +1,8 @@
 <div class="p-introduce">
-<div class="p-introduce__item">
-<p class="p-introduce__item__ttl">一緒にお仕事しませんか？</p>
-<p class="p-introduce__item__text">メールフォームより受付中！</p>
-<p class="p-introduce__item__btn c-btn--secondary--orange">
-<a href="/input">問い合わせしてみる</a></p>
-</div>
-</div>
+	<div class="p-introduce__item">
+	<p class="p-introduce__item__ttl">案件に関するご相談はこちら</p>
+		<p class="p-introduce__item__text">メールフォームより受付中！</p>
+		<p class="p-introduce__item__btn c-btn--secondary--orange">
+			<a href="/input">一緒にお仕事しませんか？</a></p>
+		</div>
+	</div>
